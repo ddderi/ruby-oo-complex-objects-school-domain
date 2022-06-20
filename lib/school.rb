97acde42a3dacc16d4df58b0cf36b46162b1e012
ddderi <maxime830@live.fr>
2022-06-20 23:45:42 +0200

    commit tree 87eb39d3519b49c30ac639aaefdd6e43dd0db191

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School 
+    attr_reader :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade] == nil
+        @roster[grade] = []
+        @roster[grade] << name
+        else
+        @roster[grade] << name
+        end
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        newh = {}
+        roster.each do |x, y|
+            newh[x] = y.sort
+        end
+        newh
+
+    end
+end
